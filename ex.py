b236d85ef370d3aeb234cd1c74c6c131ffc1763d
Sandesh bhatta <158311061+sandeshbhatta495@users.noreply.github.com>
2025-01-11 06:35:32 +0545

    Delete ex.py

--- ex.py
+++
@@ -1,5 +0,0 @@
-principal=input("Enter the value of principal:")
-rate=input("Enter the rate :")
-time=input("Enter the Time  in yrs:")
-si=float(principal*time*rate)/100
-print("The Simple Intrest of given data :",si)
