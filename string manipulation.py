994db065db3ccf4366cfb8c4346f2880dc836ba5
Sandesh bhatta <158311061+sandeshbhatta495@users.noreply.github.com>
2025-01-11 06:38:23 +0545

    Delete string manipulation.py

--- string manipulation.py
+++
@@ -1,5 +0,0 @@
-#string manipulation addtion of string like 
-print("I am doing string manipulatin")
-print("I will be try"+"and also doing python")
-print("mr.sandesh Bhatta \n are you ready?")
-input("What is your name ?")
