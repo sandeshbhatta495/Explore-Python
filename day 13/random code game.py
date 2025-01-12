eecf68e4282f9027eb999b676dd39d99ca421849
Sandesh bhatta <158311061+sandeshbhatta495@users.noreply.github.com>
2025-01-12 17:49:19 +0545

    Add files via upload

---
+++ day 13/random code game.py
@@ -0,0 +1,7 @@
+import random
+
+# Generate a random 6-digit number
+random_value = random.randint(100000, 999999)
+
+print(random_value)
+#this program generally generetes the 6 digit code which like more intersting 
