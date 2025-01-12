eca54421af295af30c422a138c430035759664bb
Sandesh bhatta <158311061+sandeshbhatta495@users.noreply.github.com>
2025-01-12 17:45:05 +0545

    Add files via upload

---
+++ day 38/dash board.py
@@ -0,0 +1,13 @@
+from tkinter import*
+
+class RMS:
+    def _init(self,root):
+        pass
+
+    if __name__=="_main":
+        root=Tk()
+        obj= RMS(root) 
+        root.mainloop
+
+
+        
